0453e6b823bfe77d185193ae318d57d5699e0241
whittingtonruth <whittington.ruth@gmail.com>
2021-01-04 14:33:30 -0600

    Edits to original gather function copied from TerrADactyl. Edits remove and add fields that are different than those used in Terrestrial AIM data.

--- R/gather_lentic.R
+++ R/gather_lentic.R
@@ -27,7 +27,6 @@ gather_lpi_lentic <- function(dsn) {
   lpi_hits_tall <- lpi_detail %>%
     dplyr::mutate_if(is.factor, as.character) %>%
     dplyr::select(
-      "parentglobalid",
       "RecKey",
       "PointNbr",
       "PointLoc",
@@ -47,7 +46,6 @@ gather_lpi_lentic <- function(dsn) {
     !is.na(code),
     code != "",
     code != "None",
-    !is.na(parentglobalid),
     !is.na(RecKey)
   )
 
@@ -55,7 +53,6 @@ gather_lpi_lentic <- function(dsn) {
   lpi_chkbox_tall <- lpi_detail %>%
     dplyr::mutate_if(is.factor, as.character) %>%
     dplyr::select(
-      "parentglobalid",
       "RecKey",
       "PointNbr",
       "PointLoc",
@@ -71,8 +68,8 @@ gather_lpi_lentic <- function(dsn) {
   # Remove woody and herbaceous checkboxes
   lpi_chkbox_tall <- lpi_chkbox_tall[!(lpi_chkbox_tall$layer %in%
                                          c("ChkboxWoody",
-                                           "ChkboxHerbaceous",
-                                           "ChkboxHerbaceous2"
+                                           "ChkboxWoody2",
+                                           "ChkboxHerbaceous"
                                          )), ]
 
   ## Make the names in the layer variable match
@@ -82,7 +79,7 @@ gather_lpi_lentic <- function(dsn) {
                                 )
 
   lpi_chkbox_tall$layer[lpi_chkbox_tall$layer == "Top"] <- "TopCanopy"
-  lpi_chkbox_tall$layer[lpi_chkbox_tall$layer == "Soil"] <- "SoilSurface"
+  lpi_chkbox_tall$layer[lpi_chkbox_tall$layer == "Basal"] <- "SoilSurface"
 
   # Print update because this can take a while
   message("Merging LPI Header and LPI Detail tables")
@@ -92,14 +89,14 @@ gather_lpi_lentic <- function(dsn) {
     x = lpi_hits_tall,
     y = lpi_chkbox_tall,
     all.x = TRUE,
-    by = c("parentglobalid", "RecKey", "PointLoc", "PointNbr", "layer")
+    by = c("RecKey", "PointLoc", "PointNbr", "layer")
   ) %>%
     dplyr::left_join(
       x = dplyr::select(
-        lpi_header, "PlotID", "PlotKey", "LineKey":"LineLengthCM", "globalid"
+        lpi_header, "PlotID", "PlotKey", "LineKey":"LineLengthCM"
       ),
       y = .,
-      by = c("globalid" = "parentglobalid")
+      by = c("LineKey" = "RecKey")
     )
   )
 
@@ -124,12 +121,17 @@ gather_species_inventory_lentic <- function(dsn) {
     ))
 
   # Make the species detail table tall
-  species_detail_tall <- tall_species_lentic(species_inventory_detail = species_inventory_detail)
+  species_detail_tall <- species_inventory_detail %>%
+    dplyr::filter(!(Species %in% c("", NA))) %>%
+    dplyr::select(
+      "RecKey",
+      "Species")
+
 
   # Join the detail table to the header and remove any NAs
   species_inventory_tall <- dplyr::left_join(x = species_inventory_header,
                                              y = species_detail_tall,
-                                             by = c("globalid" = "parentglobalid")
+                                             by = c("PlotKey" = "RecKey")
   ) %>%
     dplyr::select(., -globalid) %>%
     subset(!is.na(Species))
@@ -137,31 +139,7 @@ gather_species_inventory_lentic <- function(dsn) {
   return(species_inventory_tall)
 }
 
-# This function is called within gather_species_inventory_lentic to gather the Species Inventory detail data
-# and reformat it for use in later functions. It's main purpose seems to be to separate concatenated species fields
-# which may not be an issue for lentic. This could likely be put directly into the gather_species_inventory function.
-#' @export tall_species_lentic
-#' @rdname gather_lentic
-tall_species_lentic <- function(species_inventory_detail) {
-  tall_list <- lapply(1:nrow(species_inventory_detail), FUN = function(X,df) {
-    # split species strings concatenated in a single field
-    codes <- stringr::str_split(df[X, "Species"], pattern = ";")[[1]]
-
-    # Format output
-    output <- data.frame("parentglobalid" = df$parentglobalid[X],
-                         "RecKey" = df$RecKey[X],
-                         "Species" = codes)
-    return(output)
-
-  }, df = species_inventory_detail)
-  # Combine output and filter out any NA or blank species values
-  output <- dplyr::bind_rows(tall_list) %>%
-    dplyr::filter(!(Species %in% c("", NA)))
-
-  return(output)
-}
-
-#' @export gather_height_lentic
+ #' @export gather_height_lentic
 #' @rdname gather_lentic
 gather_height_lentic <- function(dsn){
   # Make sure geodatabase file exists
@@ -181,14 +159,11 @@ gather_height_lentic <- function(dsn){
     stringsAsFactors = FALSE
   ))
 
-  levels <- rlang::quos(globalid)
-
   # We only want to carry a subset of the lpi_header fields forward
-  lpi_header <- dplyr::select(lpi_header, !!!levels, PlotID, PlotKey, LineKey:LineLengthCM)
+  lpi_header <- dplyr::select(lpi_header, PlotID, PlotKey, LineKey:LineLengthCM)
 
   lpi_height_tall_woody <- dplyr::select(
     .data = lpi_detail,
-    parentglobalid,
     PointLoc,
     PointNbr,
     RecKey,
@@ -201,9 +176,22 @@ gather_height_lentic <- function(dsn){
     replacement = ""
   )
 
+  lpi_height_tall_woody2 <- dplyr::select(
+    .data = lpi_detail,
+    PointLoc,
+    PointNbr,
+    RecKey,
+    dplyr::matches("Woody2$")
+  ) %>% dplyr::mutate(type = "woody2", GrowthHabit_measured = "Woody")
+  # Strip out the extra name stuff so woody and herbacious variable names match
+  names(lpi_height_tall_woody2) <- stringr::str_replace_all(
+    string = names(lpi_height_tall_woody2),
+    pattern = "Woody2$",
+    replacement = ""
+  )
+
   lpi_height_tall_herb <- dplyr::select(
     .data = lpi_detail,
-    parentglobalid,
     PointLoc,
     PointNbr,
     RecKey,
@@ -215,12 +203,13 @@ gather_height_lentic <- function(dsn){
     replacement = ""
   )
 
-  #Merge all three together ##(this will be 2 woodies and 1 herbaceous in the fall)
+  #Merge all three together
   lpi_height <- rbind(
     lpi_height_tall_woody,
+    lpi_height_tall_woody2,
     lpi_height_tall_herb
   ) %>% dplyr::full_join(
-    x = lpi_header, y = ., by = c("globalid" = "parentglobalid")) %>%
+    x = lpi_header, y = ., by = c("LineKey" = "RecKey")) %>%
     subset(., !is.na(Height))
 
   # Add NA to fields with no species (i.e. the species that were "None")
@@ -236,8 +225,3 @@ gather_height_lentic <- function(dsn){
   # Output the woody/herbaceous level data
   return(lpi_height)
 }
-
-
-
-
-
