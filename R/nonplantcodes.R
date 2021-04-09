c38ac0e78e0ba37a2cb1efe36bddb06554715577
unknown <rwhit@CNHP2K.ColoState.EDU>
2021-04-09 15:10:16 -0500

    Adding all non-plant codes used in LPI in internal table.

---
+++ R/nonplantcodes.R
@@ -0,0 +1,10 @@
+#' Non-plant codes used during LPI
+#'
+#' List of codes and their labels used in LPI. In 2020, this consisted of 20
+#' codes.
+#'
+#' @docType data
+#'
+#' @usage data(nonplantcodes)
+#'
+#' @format An object of class \code{"data.frame"}
