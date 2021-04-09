25f571683d480316bd79ffbd024632554659a997
unknown <rwhit@CNHP2K.ColoState.EDU>
2021-04-09 15:12:24 -0500

    Added qc function to check that all species seen in LPI and Woody species were also in the species richness table. Additionally finished script to compile data from a header to the species list and master species list data.

--- R/species_list.R
+++ R/species_list.R
@@ -9,13 +9,73 @@
 #' # Get a list of all species occurring on a plot across methods (LPI, height, species inventory)
 #' # This method also adds cover and height by species. Be aware that sample sizes may be insufficient to make an accurate estimate
 
+#' @export species_QC_lentic
+#' @rdname species_QC_lentic
+species_QC_lentic <- function(header, spp_inventory, lpi_tall, height_tall, woody_species){
+
+  #Create a species list of all unique plot-species combinations
+  specieslist <- paste(spp_inventory$PlotKey, spp_inventory$Species, sep = "_")
+
+  #First double check that all species from LPI are accounted for in Species Inventory.
+  lpi_missingspecies <- lpi_tall%>%
+    dplyr::filter(!code %in% nonplantcodes$code)%>%
+    dplyr::mutate("PlotSpecies" = paste(PlotKey, code, sep = "_"))%>%
+    dplyr::distinct(PlotSpecies, .keep_all = F)%>%
+    dplyr::filter(!PlotSpecies %in% specieslist)
+
+  if(nrow(lpi_missingspecies > 0)){
+    warning("Species were found in LPI that were missing from Species Richness. Fix this error before producing a full species list.")
+    print(lpi_missingspecies)
+  }
+
+  #Check that all species in LPI heights are on the species list.
+  height_missingspecies <- as.data.frame(height_tall)%>%
+    dplyr::filter(Species != "N")%>%
+    dplyr::mutate("PlotSpecies" = paste(PlotKey, Species, sep = "_"))%>%
+    dplyr::distinct(PlotSpecies, .keep_all = F)%>%
+    dplyr::filter(!PlotSpecies %in% specieslist)
+
+  if(nrow(height_missingspecies > 0)){
+    warning("Species were found in LPI heights that were missing from Species Richness. Fix this error before producing a full species list.")
+    print(height_missingspecies)
+  }
+
+  #Check that all woody species are on the species list
+  woody_missingspecies <- as.data.frame(woody_species)%>%
+    dplyr::filter(RiparianWoodySpecies != "N"&RiparianWoodySpecies!="")%>%
+    dplyr::mutate("PlotSpecies" = paste(PlotKey, RiparianWoodySpecies, sep = "_"))%>%
+    dplyr::distinct(PlotSpecies, .keep_all = F)%>%
+    dplyr::filter(!PlotSpecies %in% specieslist)
+
+  if(nrow(woody_missingspecies > 0)){
+    warning("Species were found in Woody Species Form that were missing from Species Richness. Fix this error before producing a full species list.")
+    print(woody_missingspecies)
+  }
+
+  return("If no warning messages are returned, all species in LPI, height, and woody species are found in Species Richness.")
+
+}
+
 
 #' @export species_list_lentic
 #' @rdname species_list_lentic
-species_list_lentic <- function(header, spp_inventory_tall, lpi_tall, height_tall, woody_species){
-  plot <- readRDS(header)
-  spp_inventory <- readRDS(spp_inventory_tall) %>%
-    dplyr::select
-  lpi_detail <- readRDS(lpi_tall)
+species_list_lentic <- function(header, spp_inventory_tall, masterspecies){
+
+  masterlist <- masterspecies%>%
+    dplyr::select(Symbol,
+                  Scientific.Name)
+
+  spp_inventory_join <- as.data.frame(spp_inventory_tall) %>%
+    dplyr::select(PlotID,
+                  PlotKey,
+                  Species,
+                  abundance,
+                  UnknownCode)%>%
+    dplyr::mutate(UnknownCode=replace(UnknownCode, is.na(UnknownCode), ""))%>%
+    dplyr::left_join(masterlist, ., by = c("Symbol" = "Species"))%>%
+    dplyr::left_join(as.data.frame(header), .)
+
+  return(spp_inventory_join)
 
 }
+
