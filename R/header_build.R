32a6182bd1b76295ed5abe87b40c860ef34bcb8f
whittingtonruth <whittington.ruth@gmail.com>
2021-12-14 18:57:59 -0500

    Visit type pulled from field visits. New words used to filter data appropriately. PlotKey->EvaluationID.

--- R/header_build.R
+++ R/header_build.R
@@ -11,6 +11,13 @@ header_build_lentic <- function(dsn, ...) {
   # Set up filter expression (e.g., filter on PlotKey, SpeciesState, etc)
   filter_exprs <- rlang::quos(...)
 
+  #FieldVists has the correct visit date
+  fieldvisits <- sf::st_read(dsn = dsn, layer = "FieldVisits",
+                             stringsAsFactors = FALSE)%>%
+    as.data.frame()%>%
+    dplyr::filter(VisitType=="Full Sample Visit"|VisitType=="Calibration Visit")%>%
+    dplyr::distinct(StaticEvaluationID, .keep_all = T)
+
   # tblPlots provides the link between species tables
   # (LPI, Height, Species Richness) and tblStateSpecies
   header <- sf::st_read(dsn = dsn, layer = "Plots",
@@ -18,37 +25,40 @@ header_build_lentic <- function(dsn, ...) {
     as.data.frame() %>%
 
     # Filter using the filtering expression specified by user
-    dplyr::filter(!!!filter_exprs, EvalStatus == "Eval")%>%
+    dplyr::filter(!!!filter_exprs, EvalStatus == "Sampled")%>%
+    dplyr::select(-SiteName)%>%
 
-    # Remove SamplingApproach from Plots, as format in PlotChar is closer to what we want to use.
-    dplyr::select(-SamplingApproach)
+    #Add field visits
+    dplyr::left_join(.,
+                     fieldvisits%>%dplyr::select(PlotID, EvaluationID = StaticEvaluationID, FieldEvalDate, VisitType),
+                     by = c("PlotID" = "PlotID"))
 
   #some fields can be brought in from Plot Characterization to expand information
   plotcharfields <- sf::st_read(dsn = dsn, layer = "PlotCharacterization",
                                 stringsAsFactors = FALSE) %>%
     as.data.frame() %>%
 
-    dplyr::select(PlotKey,
+    dplyr::select(PlotID,
+                  EvaluationID,
                   SiteName,
-                  SamplingApproach,
-                  Northing,
-                  Easting,
+                  Latitude,
+                  Longitude,
                   PlotLayout,
                   ElevationCtr)
 
   # Join two tables and remove unnecessary fields.
-  header <- dplyr::left_join(header, plotcharfields) %>%
+  header <- dplyr::left_join(header, plotcharfields, by = c("PlotID", "EvaluationID")) %>%
     dplyr::select(PlotID,
-                  PlotKey,
+                  EvaluationID,
                   SiteName,
                   SamplingApproach,
                   AdminState,
-                  Region,
-                  DistrictOffice = District,
-                  FieldOffice = Office,
-                  VisitDate,
-                  LatWGS = Northing,
-                  LongWGS = Easting,
+                  WetlandIndicatorRegion,
+                  DistrictOffice,
+                  FieldOffice,
+                  FieldEvalDate,
+                  LatWGS = Latitude,
+                  LongWGS = Longitude,
                   Elevation = ElevationCtr
     )
 
